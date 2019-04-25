6b5571c62c2f58808209430924c58377169dbfe6
Stephanie Sapp <stephs1531@gmail.com>
2019-04-24 20:33:05 -0500

    got the quotes api working!

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,25 +2,25 @@
 
 
 //set API key and topic variables
-var APIKey = "c615aa6dedmsh39f89562dade0d8p122543jsnc8984b13db35"
+    var APIKey = "c615aa6dedmsh39f89562dade0d8p122543jsnc8984b13db35"
    var topic = "Wisdom"
 
    //insert topic and API key into queryURL
-  var queryURL = "https://healthruwords.p.rapidapi.com/v1/quotes/?id=731&t=" + topic + "&maxR=1&size=medium&key=" + APIKey;
+   var queryURL = "https://quotable-quotes.p.rapidapi.com/randomQuotes"
     
-        // We then created an AJAX call
-        $.ajax({
-        url: queryURL,
-        method: "GET",
-        headers: {"X-RapidAPI-Host": "healthruwords.p.rapidapi.com", "X-RapidAPI-Key": "c615aa6dedmsh39f89562dade0d8p122543jsnc8984b13db35"}
-        }).then(function(response) {
-        console.log(response)
+   // We then created an AJAX call
+    $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {"X-RapidAPI-Host": "quotable-quotes.p.rapidapi.com", "X-RapidAPI-Key": "6f84e5eceamsh5c5d979bf355c11p1f651ajsneb75d52cb90c"}
+    }).then(function(response) {
+    console.log(response)
         
-        var quoteImage = response.url;
-        var author = $("<h3>").text(response.author)
+    var quote = $("<p>").text(response.quote);
+    var author = $("<h3>").text("– " + response.author)
 
-        $(".quoteText").prepend(quoteImage);
-        $(".quoteAuthor").prepend(author);
+    $(".quoteText").prepend(quote);
+    $(".quoteAuthor").prepend(author);
 
 
         });
