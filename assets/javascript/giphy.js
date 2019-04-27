af2c079f902cd8143dab129adfc79c20a7c1e2a3
Stephen Malito <stevemalito@gmail.com>
2019-04-27 09:34:43 -0500

    Giphy use mood choice

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,6 +1,7 @@
- 
+$(".dropdown-menu a").on("click", function () { 
  // var feeling = "happy"
- var queryURL = "http://api.giphy.com/v1/gifs/search?q=happy&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=10";
+ var feeling = $(this).text();
+ var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + feeling + "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=10";
 
  // We then created an AJAX call
  $.ajax({
@@ -18,3 +19,4 @@
     //  Still missing code to randomly choose gif from results 
     
  });
+});
