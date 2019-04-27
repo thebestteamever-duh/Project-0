45c2210ae173eb3813bf9ceb678d545fdc18df39
ElyriaG <ElyriaG@users.noreply.github.com>
2019-04-26 20:15:42 -0500

    formatted weather js

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -10,41 +10,26 @@ $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function (response) {
-  console.log(response.weather[0].description)
-  console.log(response)
-  // Create CODE HERE to Log the queryURL
-
-  // Create CODE HERE to log the resulting object
-
-  // Create CODE HERE to transfer content to HTML 
-  let wDesc = response.weather[0].description;
-  let temp = response.weather[0].description;
 
 
   // Variables for Weather Information
+  let wDesc = response.weather[0].description;
+  let temp = response.weather[0].description;
+
   var icon = response.weather[0].icon
   var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png"
-  var tempBase = Math.round(response.main.temp);
 
+  var tempBase = Math.round(response.main.temp);
 
   var sunset = response.sys.sunset
   var time = moment.unix(sunset).format("HH: mm");
-  console.log(time)
-  $("#sunset").text(time);
-
-
-
-
-
-
-
-
+  
 
   // Create CODE HERE to dump the temperature content into HTML
+  $("#sunset").text(time);
   $("#temp").text(tempBase);
   $("#weatherDescription").text(response.weather[0].description);
   $("#humidity").text(response.main.humidity);
-  // $("#sunset").text(response.sys.sunset);
   $(".fas").attr("src", iconUrl);
 });
 
