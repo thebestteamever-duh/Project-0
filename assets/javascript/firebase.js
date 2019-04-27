103907ca5c0f28cf0a9a6b2450cb55fc6c1e825d
Stephen Malito <stevemalito@gmail.com>
2019-04-27 11:24:32 -0500

    Create firebase.js

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA9II18kgiRBsKko3xJVcsQ5ai9PsNJtMY",
+    authDomain: "mindset-c4818.firebaseapp.com",
+    databaseURL: "https://mindset-c4818.firebaseio.com",
+    projectId: "mindset-c4818",
+    storageBucket: "mindset-c4818.appspot.com",
+    messagingSenderId: "4140115652"
+  };
+  firebase.initializeApp(config);
