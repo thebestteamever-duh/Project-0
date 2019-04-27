6bef48dd4c66e5f600d76f0f7323c7a391998d1f
ElyriaG <ElyriaG@users.noreply.github.com>
2019-04-27 11:52:40 -0500

    F symbol, multiple weather descriptions

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -3,7 +3,7 @@ var APIKey = "c8bcf8bb7a6d8ef744d855a989993230";
 
 
 // Here we are building the URL we need to query the database
-var queryURL = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=chicago,illinois&units=imperial&appid=" + APIKey;
+var queryURL = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=paris,fr&units=imperial&appid=" + APIKey;
 
 // We then created an AJAX call
 $.ajax({
@@ -13,8 +13,7 @@ $.ajax({
 
 
   // Variables for Weather Information
-  let wDesc = response.weather[0].description;
-  let temp = response.weather[0].description;
+  var wDesc = response.weather[0].description;
 
   var icon = response.weather[0].icon
   var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png"
@@ -23,13 +22,24 @@ $.ajax({
 
   var sunset = response.sys.sunset
   var time = moment.unix(sunset).format("HH: mm");
-  
+
 
   // Code to insert weather information onto HTML page
   $("#sunset").text(time);
-  $("#temp").text(tempBase);
-  $("#weatherDescription").text(response.weather[0].description);
+  $("#temp").text(tempBase + "℉");
   $("#humidity").text(response.main.humidity);
   $(".fas").attr("src", iconUrl);
+
+
+  // For 2 weather descriptions
+  if (response.weather.length > 1) {
+    var wDesc2 = response.weather[1].description;
+    $("#weatherDescription").append(wDesc + " & " + wDesc2);
+  }
+  else {
+    $("#weatherDescription").append(wDesc);
+  }
+
+  console.log(response);
 });
 
