4920039f479f07a4b11b6465ba0a40ae3bc9847c
Stephen Malito <stevemalito@gmail.com>
2019-04-27 11:58:26 -0500

    Firebase.js - if function started

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -8,3 +8,42 @@
     messagingSenderId: "4140115652"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $(".dropdown-menu a").on("click", function () {
+    event.preventDefault();
+
+    var topic = $(this).text();
+
+    //mood choice variables
+    var happy = 0;
+    var sad = 0;
+    var angry = 0;
+    var scared = 0;
+    var tired = 0;
+    var love = 0;
+
+    //*******Moods******* */
+    //    "Happy"
+    //    "Sad"
+    //    "Angry"
+    //     "Scared"
+    //     "Tired"
+    //     "Love"
+
+//Create an if Function to increment 
+    if (topic = "Happy"){ 
+        happy++
+    } else if (topic = "Sad"){
+        sad++
+    }
+
+  
+        
+
+
+
+
+
+  });    
