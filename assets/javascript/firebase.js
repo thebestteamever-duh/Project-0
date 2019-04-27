62f620ff5dd50dd3bbe5c595c2118b94928f6d4f
Stephen Malito <stevemalito@gmail.com>
2019-04-27 12:39:02 -0500

    firebase setup, but not pushing

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -6,10 +6,18 @@
     projectId: "mindset-c4818",
     storageBucket: "mindset-c4818.appspot.com",
     messagingSenderId: "4140115652"
+
   };
   firebase.initializeApp(config);
 
   var database = firebase.database();
+  
+  var happy = 0;
+  var sad = 0;
+  var angry = 0;
+  var scared = 0;
+  var tired = 0;
+  var love = 0;
 
   $(".dropdown-menu a").on("click", function () {
     event.preventDefault();
@@ -17,12 +25,7 @@
     var topic = $(this).text();
 
     //mood choice variables
-    var happy = 0;
-    var sad = 0;
-    var angry = 0;
-    var scared = 0;
-    var tired = 0;
-    var love = 0;
+   
 
     //*******Moods******* */
     //    "Happy"
@@ -46,11 +49,20 @@
     } else {
         love++
     }
-  
-        
+
+  database.ref().set({
+    fHappy: happy,
+      fSad: sad,
+      fAngry: angry,
+      fScared: scared,
+      fTired: tired,
+      fLove: love,
+  });
+
+});
 
 
 
 
 
-  });    
+ 
