c4d147b40b75a21e16017c9aa4048ae8f2c389f1
ElyriaG <ElyriaG@users.noreply.github.com>
2019-04-27 13:43:19 -0500

    weather widget pretty

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,7 +2,7 @@ $(".dropdown-menu a").on("click", function () {
  // var feeling = "happy"
  $(".gif").empty()
  var feeling = $(this).text();
- var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + feeling + "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=10";
+ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + feeling + "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=20";
 
     // We then created an AJAX call
     $.ajax({
@@ -10,11 +10,11 @@ $(".dropdown-menu a").on("click", function () {
         method: "GET",
     }).then(function (response) {
         var random = Math.floor(Math.random() * 21);
-        // console.log(response);
+        console.log("RESPONSE " + results);
 
         var results = response.data[random];
         var feelingGif = $("<img>").addClass("gif");
-        feelingGif.attr("src", results.images.fixed_height.url);
+        feelingGif.attr("src", results.images.original.url);
 
         $(".gif").append(feelingGif);
     });
