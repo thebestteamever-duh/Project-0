2359f413e317368c6405a16c81d9f68d50fe8576
ElyriaG <ElyriaG@users.noreply.github.com>
2019-05-01 21:45:21 -0500

    formatting

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,8 +1,8 @@
-$(".dropdown-menu a").on("click", function () { 
- 
- $(".gif").empty()
- var feeling = $(this).text();
- var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + feeling + "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=20";
+$(".dropdown-menu a").on("click", function () {
+
+    $(".gif").empty()
+    var feeling = $(this).text();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + feeling + "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=20";
 
     // We then created an AJAX call
     $.ajax({
